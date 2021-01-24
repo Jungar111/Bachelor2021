0ea7b59542f8332a1f976a3a756dc15e309a13f6
FrederikMS <Frederik.moller2@hotmail.com>
2021-01-24 16:54:47 +0100

    Test fejl!

--- Dundee_Data_Vis.py
+++ Dundee_Data_Vis.py
@@ -1,4 +1,16 @@
+import pandas as pd 
+import matplotlib.pyplot as plt
+
+dec_mar = pd.read_csv("data\Dundee\cp-data-dec-mar-2018.csv")
+mar_may = pd.read_csv("data\Dundee\cp-data-mar-may-2018.csv")
+
+data = dec_mar.append(mar_may)
+
+print(data.head())
+#print(data.isna().sum())
+#print(data.shape)
 
 
-
+plt.plot(data["Total kWh"])
+plt.show()
 

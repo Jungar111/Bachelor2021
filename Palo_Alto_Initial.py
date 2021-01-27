8e13ae0dd98e2d7651878e41fdd0867cb198ba74
FrederikMS <Frederik.moller2@hotmail.com>
2021-01-27 12:31:57 +0100

    + soruce dataset (Chargepoint) and looking at it

--- Palo_Alto_Initial.py
+++ Palo_Alto_Initial.py
@@ -1,12 +1,18 @@
 import pandas as pd 
 padata = pd.read_csv("data\PaloAltoData.csv")
 padata2 = pd.read_csv("data\PaloAltoData2.csv")
+cpdata = pd.read_csv("data\ChargePoint Data 2017Q4.csv")
 
 #print(padata.head())
-print(padata.isna().sum())
+print(cpdata.isna().sum())
 
-print(padata["MAC Address"].unique())
-#print(padata2.shape)
+print(len(cpdata["Longitude"].unique()))
+#print(cpdata.shape)
 
 # I dont see a huge difference in the two links? and still only 7 chargers??? and 5 MAC Adresses??
 # the same latitudes and longitudes 
+
+
+# found source of second link, and there seems to be much more data here. However seems to be some descrepencies between how 
+# many chargers there are depending on Long/lat, Station Name and MAC Address? Which sould we use?
+# Otherwise there dont seem to be problems with nans in the "important" categories

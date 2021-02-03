f68e7e9577babf482875acd11d0e9f1bc023fb7b
FrederikMS <Frederik.moller2@hotmail.com>
2021-02-03 11:27:48 +0100

    Flere plots + cleanup

--- Palo_Alto_Initial.py
+++ Palo_Alto_Initial.py
@@ -10,12 +10,30 @@ class clean_paloalto:
     
     def clean_data(self):
         # We can drop EVSE ID, since mac address has more obs. 
-        self.data=self.pa_data.drop(["Address 2","EVSE ID","County","System S/N","Model Number"],axis=1)
+        self.data=self.pa_data.drop(["Address 2","EVSE ID","County","System S/N","Model Number","Transaction Date (Pacific Time)"],axis=1)
         self.data=self.data.dropna()
+        self.data.index=range(len(self.data))
+        self.to_date(self.data)
+        self.to_float(self.data)
+        self.drop_strangedata(self.data)
         return self.data
     
-    def normdistr(self):
-        data = self.clean_data()
+    def drop_strangedata(self,df):
+        df["Charge Duration (mins)"]=df[df["Charge Duration (mins)"]!=" 1,104 "]
+    
+    def to_float(self,df):
+        df["Charge Duration (mins)"][df["Charge Duration (mins)"]==" -   "]=0
+        df["Charge Duration (mins)"]=pd.to_numeric(df["Charge Duration (mins)"])
+    
+    def to_date(self,df):
+        df["Start Date"]=pd.to_datetime(df["Start Date"],format="%m/%d/%Y %H:%M", errors="coerce")
+        df["End Date"]=pd.to_datetime(df["End Date"],format="%m/%d/%Y %H:%M", errors="coerce")
+        df["Total Duration (hh:mm:ss)"]=pd.to_datetime(df["Total Duration (hh:mm:ss)"],format="%H:%M:%S")
+        df["Charging Time (hh:mm:ss)"]=pd.to_datetime(df["Charging Time (hh:mm:ss)"],format="%H:%M:%S")
+        
+
+class viz:
+    def normdistr(self,data):
         columns = ["Energy (kWh)","GHG Savings (kg)","Gasoline Savings (gallons)","Fee"]
         for col in columns:
             mean, var  = scipy.stats.distributions.norm.fit(data[col])
@@ -26,8 +44,7 @@ class clean_paloalto:
             plt.plot(x,fitted_data,'r-')
             plt.show()
     
-    def expdistr(self):
-        data = self.clean_data()
+    def expdistr(self, data):
         columns = ["Energy (kWh)","GHG Savings (kg)","Gasoline Savings (gallons)","Fee"]
         for col in columns:
             mean, var  = scipy.stats.distributions.expon.fit(data[col])
@@ -38,30 +55,36 @@ class clean_paloalto:
             plt.plot(x,fitted_data,'r-')
             plt.show()
     
-    def lognormdistr(self):
-        data = self.clean_data()
+    def lognormdistr(self,data):
         columns = ["Energy (kWh)","GHG Savings (kg)","Gasoline Savings (gallons)","Fee"]
         for col in columns:
-            mean, var, skew  = scipy.stats.distributions.lognorm.fit(data[col])
-            x = np.linspace(0.0000001,mean+np.sqrt(var)*6,100)
-            fitted_data = scipy.stats.distributions.lognorm.pdf(x, mean, var, skew)
-
             plt.hist(data[col], density=True, bins=100)
-            plt.plot(x,fitted_data,'r-')
+            sns.distplot(data[col], fit=scipy.stats.lognorm)
             plt.show()
 
-    def pairsplot(self):
-        data = self.clean_data()
+    def pairsplot(self,data):
         sns.pairplot(data[["Energy (kWh)","GHG Savings (kg)","Gasoline Savings (gallons)","Port Number","Postal Code","Fee"]])
+        plt.show()
+
+    def basisplots(self,data):
+        fig, ax = plt.subplots(2,2)
+        ax[0,0].scatter(data["MAC Address"],data["Energy (kWh)"],s=0.1) 
+        ax[0,1].scatter(data["MAC Address"],data["Charge Duration (mins)"],s=0.1) 
+        ax[1,0].scatter(data["MAC Address"],data["Gasoline Savings (gallons)"],s=0.1) 
+        ax[1,1].scatter(data["MAC Address"],data["Fee"],s=0.1) 
+        plt.show()
+
+        plt.scatter(data["Start Date"],data["Energy (kWh)"],s=0.1)
         plt.show()
 
 
 if __name__=='__main__':
     c = clean_paloalto()
+    v = viz()
     data = c.clean_data()
+    v.basisplots(data)
+    #print(data["Energy (kWh)"].dtypes)
     #c.pairsplot()
     #c.expdistr()
-    c.lognormdistr()
-    #print(math.log(data["Energy (kWh)"]))
-    #print(data["Energy (kWh)"].describe())
-    #print(data.apply(lambda r : math.exp(r["Energy (kWh)"]),1))
+    #v.lognormdistr(data)
+

d6dddc631f14eb51105ee714882ca1618c9dae35
Jungar <asger@tang.dk>
2021-02-19 10:52:38 +0100

    clusters

--- grid.py
+++ grid.py
@@ -3,9 +3,9 @@ import folium
 from sklearn.cluster import KMeans
 
 class gridmap:
-    def grid(self,df):
+    def grid(self,df, c):
 
-        kmeans = KMeans(n_clusters=6, random_state=0).fit(df[["Latitude","Longitude"]])
+        kmeans = KMeans(n_clusters=c, random_state=0).fit(df[["Latitude","Longitude"]])
         clusters=kmeans.cluster_centers_
         return clusters
         

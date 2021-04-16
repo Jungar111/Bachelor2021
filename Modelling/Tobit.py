1cf58a25ed2209a8dede0e826d221cb5f7152712
Jungar <asger@tang.dk>
2021-04-16 15:16:58 +0200

    more tobit

--- Modelling/Tobit.py
+++ Modelling/Tobit.py
@@ -20,26 +20,26 @@ class Tobit:
         pred = regressor.predict(np.array(self.df[self.X][self.df[self.censored] == censoring]).reshape(-1,1))
         return pred
 
-    def nll(self, sd):
+    def nll(self, sd, upper_threshold):
         y_not_censored = self.df[self.y][self.df[self.censored] == False]
         y_pred_not_censored = self.linear_regression(False)
 
         ll_not_censored = tf.math.reduce_sum(np.log(1/sd * norm.pdf((y_not_censored - y_pred_not_censored)/sd)))
 
         y_censored = self.df[self.y][self.df[self.censored] == True]
-        y_pred_censored = self.linear_regression(True)
-        ll_censored = tf.math.reduce_sum(np.log(norm.cdf((y_censored - y_pred_censored)/sd)))
+        #y_pred_censored = self.linear_regression(True)
+        ll_censored = tf.math.reduce_sum(np.log(norm.cdf((y_censored - upper_threshold)/sd)))
 
         return float(- ll_not_censored - ll_censored)
 
-    def minimize(self, initial_guess):
-        return minimize(self.nll, initial_guess, method = "BFGS")
+    def minimize(self, initial_guess,upper_threshold):
+        return minimize(self.nll, initial_guess, method = "BFGS", args=(upper_threshold))
 
     def Lambda(self, sd, prediction):
         return norm.pdf(prediction/sd)/norm.cdf(prediction/sd)
 
     def predict(self, sd, prediction):
-        return norm.cdf(prediction/sd)*(prediction + sd* self.Lambda(sd, prediction))
+        return norm.cdf(prediction / sd) * (prediction + sd * self.Lambda(sd, prediction))
 
 
 if __name__ == "__main__":

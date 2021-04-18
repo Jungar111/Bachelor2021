9a4b38f1dcdb4fb9cd1344040a8473d3bb556bd5
Jungar <asger@tang.dk>
2021-04-18 21:42:32 +0200

    tobit

--- Modelling/Tobit.py
+++ Modelling/Tobit.py
@@ -1,4 +1,5 @@
-from scipy.stats import norm
+import scipy
+from scipy.stats import norm, lognorm
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 import numpy as np
@@ -6,6 +7,8 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 from scipy.optimize import minimize
 from sklearn.metrics import r2_score
+import tensorflow_probability as tfp
+import math
 
 class Tobit:
     def __init__(self, df, censored, x, y):
@@ -16,31 +19,88 @@ class Tobit:
     
     def linear_regression(self, censoring):
         regressor = LinearRegression()
-        regressor.fit(np.array(self.df[self.X]).reshape(-1,1), self.df[self.y])
-        pred = regressor.predict(np.array(self.df[self.X][self.df[self.censored] == censoring]).reshape(-1,1))
+        regressor.fit(np.array(self.df[self.X]), self.df[self.y])
+        pred = regressor.predict(np.array(self.df[self.X][self.df[self.censored] == censoring]))
         return pred
 
-    def nll(self, sd, upper_threshold):
-        y_not_censored = self.df[self.y][self.df[self.censored] == False]
-        y_pred_not_censored = self.linear_regression(False)
+    def nll(self, vars, upper_threshold):
+        sd = vars[-1]
+        beta = vars[:-1]
+        x_not_censored = np.array(self.df[self.X][self.df[self.censored] == False])
+        y_not_censored = np.array(self.df[self.y][self.df[self.censored] == False])
+        x_censored = np.array(self.df[self.X][self.df[self.censored] == True])
+        y_censored = np.array(self.df[self.y][self.df[self.censored] == True])
+        
+        
+        print(np.dot(x_censored, beta))
+        print((np.dot(x_censored, beta) - y_censored)/sd)
+        ll_censored = scipy.stats.norm.logcdf((np.dot(x_censored, beta) - y_censored)/sd).sum()
+        ll_not_censored = (scipy.stats.norm.logpdf(y_not_censored - np.dot(x_not_censored, beta)/sd) - math.log(max(np.finfo('float').resolution, sd))).sum()
 
-        ll_not_censored = tf.math.reduce_sum(np.log(1/sd * norm.pdf((y_not_censored - y_pred_not_censored)/sd)))
+        # y_not_censored = self.df[self.y][self.df[self.censored] == False]
 
-        y_censored = self.df[self.y][self.df[self.censored] == True]
-        #y_pred_censored = self.linear_regression(True)
-        ll_censored = tf.math.reduce_sum(np.log(norm.cdf((y_censored - upper_threshold)/sd)))
+        # ll_not_censored = tf.math.reduce_sum(np.log(1/sd) + tfp.distributions.Normal(loc = 0, scale =1).log_prob((y_not_censored - y_not_censored*beta)/sd))
+        
+        # y_censored = self.df[self.y][self.df[self.censored] == True]
+        # ll_censored = tf.math.reduce_sum(tfp.distributions.Normal(loc = 0, scale =1).log_cdf((y_censored*beta - y_censored)/sd))
+        
+        loglik = float(ll_censored + ll_not_censored)
+        return - loglik
 
-        return float(- ll_not_censored - ll_censored)
+    def con(self,sd):
+        return sd > 0
 
     def minimize(self, initial_guess,upper_threshold):
-        return minimize(self.nll, initial_guess, method = "BFGS", args=(upper_threshold))
+        return minimize(self.nll, initial_guess, method = "Nelder-Mead",args=(upper_threshold))
 
     def Lambda(self, sd, prediction):
         return norm.pdf(prediction/sd)/norm.cdf(prediction/sd)
 
     def predict(self, sd, prediction):
+        #sd = np.exp(sd)
         return norm.cdf(prediction / sd) * (prediction + sd * self.Lambda(sd, prediction))
 
 
 if __name__ == "__main__":
-    pass
+    x1 = np.linspace(0, 10, 200)
+    x2 = np.linspace(0, 10, 200)
+    y = x1*2 + x2 + np.random.normal(0,1,200)
+    y[y > 15] = 15
+
+    df = pd.DataFrame({'x1':x1, 'x2':x2, 'y':y})
+    df['censor'] = y == 15
+    t = Tobit(df, 'censor',['x1','x2'], 'y')
+
+    regressor = LinearRegression().fit(df[['x1','x2']],df['y'])
+    pred = regressor.predict(df[['x1','x2']])
+    beta = regressor.coef_
+    print(beta)
+    sd_guess = 1.5
+    # sds = np.linspace(0.001, 3, 1000)
+    # vars = [[beta, i] for i in sds]
+    
+    
+    # nlls = []
+    # for var in vars:
+    #     nlls.append(t.nll(var,15))
+    
+    # plt.plot(sds, nlls)
+    # plt.show()
+    
+    vars = np.append(beta, sd_guess)
+    print(vars)
+    mini = t.minimize(vars,15)
+    print(mini)
+    print(beta)
+    sd = mini.x[-1]
+    pred_pred = x1*mini.x[0] + x2*mini.x[1]
+    print(mini)
+    # pred_t = t.predict(np.exp(sd), pred_pred)
+
+    plt.scatter(x1,y, label="Data", color="black",alpha=0.8)
+    plt.plot(x1, pred_pred, label="Tobit", color="red", alpha=0.8)
+    plt.plot(x1, pred, label="Linear", color="green",alpha=0.8)
+    plt.legend()
+    plt.show()
+
+    

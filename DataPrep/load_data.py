7f75978b788b7f59fbe41accf2943551e0feb773
Jungar <asger@tang.dk>
2021-04-22 13:06:25 +0200

    minor

--- DataPrep/load_data.py
+++ DataPrep/load_data.py
@@ -1,12 +1,17 @@
 import sys
 sys.path.append(".")
+from pathlib import Path
 
 import pandas as pd
 
 def load_data():
-    
-    df = pd.read_csv("data/createdDat/AlmostClean.csv")
+    p = Path('data', 'createdDat', 'AlmostClean.csv')
+    df = pd.read_csv(p)
     df = df.set_index("Date")
     df.index = pd.to_datetime(df.index)
 
     return df
+
+
+if __name__ == "__main__":
+    print(load_data())

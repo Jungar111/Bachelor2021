bfa444a07e410d9e20451778d4a4d8f6357dd387
Jungar <asger@tang.dk>
2021-04-22 13:04:27 +0200

    loader created

--- DataPrep/load_data.py
+++ DataPrep/load_data.py
@@ -1,1 +1,12 @@
-import pyforest
+import sys
+sys.path.append(".")
+
+import pandas as pd
+
+def load_data():
+    
+    df = pd.read_csv("data/createdDat/AlmostClean.csv")
+    df = df.set_index("Date")
+    df.index = pd.to_datetime(df.index)
+
+    return df

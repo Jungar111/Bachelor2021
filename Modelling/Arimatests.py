874b2759c57bb0dc08e07b975b6d482965bda49b
FrederikMS <Frederik.moller2@hotmail.com>
2021-04-23 10:23:27 +0200

    minor

--- Modelling/Arimatests.py
+++ Modelling/Arimatests.py
@@ -2,7 +2,7 @@ import sys
 sys.path.append(".")
 from statsmodels.tsa.arima.model import ARIMA
 from statsmodels.tsa.statespace.sarimax import SARIMAX
-from DataPrep.ImportData import importer
+from DataPrep.load_data import load_data
 from DataPrep.LagCreation import lags
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_percentage_error, mean_absolute_error
@@ -22,23 +22,23 @@ def ArimaModels(df):
     res = pd.DataFrame(columns=["Label","ar","d","ma","AIC","r^2","RMSE","MAPE","MAE","SMAPE"])
     with tqdm(total=2400, file=sys.stdout) as pbar:
         for label in range(8):
-            df1=df[df["Label"]==float(label)]
-            df1=df1.sort_values("Start Date")
-            y = df1[["Energy (kWh)","Start Date"]].set_index("Start Date",drop=False)
+            df1=df[df["Label"]==label]
+            #df1=df1.sort_values("Start Date")
+            #y = df1[["Energy (kWh)","Start Date"]].set_index("Start Date",drop=False)
 
-            y= y.resample("d").min()
-            X = lags().buildLaggedFeatures(y["Energy (kWh)"], ["Energy (kWh)"],5, dropna=False)
-            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42, shuffle=False)
-            X_train=X_train.drop(columns="lag_0")
-            X_test=X_test.drop(columns="lag_0")
-            y_train = y_train.set_index("Start Date",drop=False)
-            y_test = y_test.set_index("Start Date",drop=False)
+            #y= y.resample("d").min()
+            #X = lags().buildLaggedFeatures(y["Energy (kWh)"], ["Energy (kWh)"],5, dropna=False)
+            #X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42, shuffle=False)
+            #X_train=X_train.drop(columns="lag_0")
+            #X_test=X_test.drop(columns="lag_0")
+            #y_train = y_train.set_index("Start Date",drop=False)
+            #y_test = y_test.set_index("Start Date",drop=False)
 
-            y_train.freq= "D"
-            y_train = y_train.resample("d").min()
-            y_test = y_test.resample("d").min()
-            y_train["Energy (kWh)"]  = y_train["Energy (kWh)"].fillna(0)
-            y_test["Energy (kWh)"]  = y_test["Energy (kWh)"].fillna(0)
+            #y_train.freq= "D"
+            #y_train = y_train.resample("d").min()
+            #y_test = y_test.resample("d").min()
+            #y_train["Energy (kWh)"]  = y_train["Energy (kWh)"].fillna(0)
+            #y_test["Energy (kWh)"]  = y_test["Energy (kWh)"].fillna(0)
 
 
             
@@ -46,17 +46,17 @@ def ArimaModels(df):
                 for d in range(3):
                     for ma in range(10):
                         
-                        sam = SARIMAX((y_train["Energy (kWh)"]),exog=X_train.fillna(0), order=(ar,d,ma), trend="n", freq="D")
+                        sam = SARIMAX((df["Energy (kWh)"][:2500]), order=(ar,d,ma), trend="n", freq="D")
                         sam_fit = sam.fit(method="lbfgs", disp = False, full_output = False)
-                        n=len(y_test["Energy (kWh)"])
-                        y_pred = sam_fit.forecast(steps = n, exog=X_test.fillna(0))
+                        n=len(df[2501:])
+                        y_pred = sam_fit.forecast(steps = n)
 
                         AIC = sam_fit.aic
-                        r = r2_score(y_test["Energy (kWh)"],y_pred)
-                        RMSE = np.sqrt(mean_squared_error(y_test["Energy (kWh)"],y_pred))
-                        MAPE = mean_absolute_percentage_error(y_test["Energy (kWh)"],y_pred)
-                        MAE = mean_absolute_error(y_test["Energy (kWh)"],y_pred)
-                        SMAPE = smape(y_test['Energy (kWh)'],y_pred)
+                        r = r2_score(df["Energy (kWh)"][2501:],y_pred)
+                        RMSE = np.sqrt(mean_squared_error(df["Energy (kWh)"][2501:],y_pred))
+                        MAPE = mean_absolute_percentage_error(df["Energy (kWh)"][2501:],y_pred)
+                        MAE = mean_absolute_error(df["Energy (kWh)"][2501:],y_pred)
+                        SMAPE = smape(df["Energy (kWh)"][2501:],y_pred)
                     
                         results = dict(zip(list(res.columns),[label,ar,d,ma,AIC,r,RMSE,MAPE,MAE,SMAPE]))
                         res = res.append(results,True)
@@ -66,7 +66,7 @@ def ArimaModels(df):
     
     return res
 
-df = importer().Import()
+df = load_data()
 
 print("Imported!")
 
--- Modelling/Sarimax_model.py
+++ Modelling/Sarimax_model.py
@@ -1,74 +1,57 @@
 import sys
 sys.path.append(".")
 
-from DataPrep.ImportData import importer
-from Modelling import modelling
 from sklearn.metrics import mean_squared_error, r2_score
-from DataPrep.LagCreation import lags
-from sklearn.model_selection import train_test_split
 from statsmodels.tsa.statespace.sarimax import SARIMAX
-from statsmodels.tsa.arima.model import ARIMA
 import warnings
 import pandas as pd
 import matplotlib.pyplot as plt
+from DataPrep.load_data import load_data
+import numpy as np
+from tqdm import tqdm
 
 warnings.filterwarnings("ignore")
 
 
-df = importer().Import()
-
-df2 = pd.DataFrame()
-
-df = df.set_index("Start Date")
-df.index = df.index.to_period("D")
-
-idx = pd.period_range(df.index.min(),df.index.max())
-for i in range(8): 
-    dat = df[df["Label"]==float(i)].reindex(idx, fill_value=0)
-    dat.Label = float(i)
-    df2 = df2.append(dat)
-
-df2 = df2.sort_index()
-df2["Start Date"] = df2.index
-
-y = df2[["Energy (kWh)","Start Date", "Label"]]
-
-y_cols = pd.DataFrame(columns=["Label 0","Label 1","Label 2","Label 3","Label 4","Label 5","Label 6","Label 7"])
-for i in range(8):
-    y_cols[f"Label {i}"]=y["Energy (kWh)"][y["Label"]==float(i)]
+df = load_data()
+df = df[df["Label"]==0]
 
 pred = []
 days_pred = []
-train = int((len(y_cols["Label 0"].index))*0.8)
-re = len(y_cols["Label 0"].index)-train
-a = y_cols.index[0].to_timestamp()
-b = y_cols.index[train].to_timestamp()
-for i in range(300):#re):
-    sam = ARIMA(y_cols["Label 0"][:b],order=(10,1,1), freq="D")
-    try:
-        sam_fit = sam.fit()
+train = int((len(df["Energy (kWh)"].index))*0.1)
+re = len(df["Energy (kWh)"].index)-train
+a = df.index[0]#.to_timestamp()
+b = df.index[train]#.to_timestamp()
+with tqdm(total=re, file=sys.stdout) as pbar:
+    for i in range(re):
+        try:
+            sam = SARIMAX(np.sqrt(df["Energy (kWh)"][:b]),order=(6,1,7), freq="D")
+            sam_fit = sam.fit(disp=-1)
+            y_pred = sam_fit.forecast()
+            pred.append(float(y_pred))
+            days=1
+            n = b + pd.Timedelta(days=days)
+            days_pred.append(n)
+        
+        except np.linalg.LinAlgError:
+            continue
 
         days = 1
-        n=b+pd.Timedelta(days=days)
-        n1 = n + pd.Timedelta(days=1)
-        y_pred = sam_fit.forecast(steps = days)
-        y_pred.index=y_pred.index.to_timestamp()
-        pred.append(float(y_pred))
-        days_pred.append(n)
-    except:
-        continue
-    #a = a + pd.Timedelta(days=days)
-    b = b + pd.Timedelta(days=days)
-    
-    print(f"Progress {i}:{re}")
+        
+        
+        a = a + pd.Timedelta(days=days)
+        b = b + pd.Timedelta(days=days)
+
+        pbar.update(1)
 
 
 
 pred = pd.DataFrame(pred)
 pred.index = days_pred
-pred.to_csv("Arimares.csv")
+pred.to_csv("data\createdDat\Arimares.csv")
 
 plt.plot(pred)
-plt.plot(y_cols["Label 0"][pred.index[0]:pred.index[-1]])
+plt.plot(df["Energy (kWh)"][pred.index[0]:pred.index[-1]])
 plt.show()
-print(r2_score(y_cols["Label 0"][pred.index[0]:pred.index[-1]],pred))
+print(f'r^2 score {r2_score(df["Energy (kWh)"][days_pred[0]:days_pred[-1]],pred**2)}')
+print(f'RMSE {np.sqrt(mean_squared_error(df["Energy (kWh)"][days_pred[0]:days_pred[-1]],pred**2))}')

d62560bace42161e96e352b9a92e3888c2cf7f91
FrederikMS <Frederik.moller2@hotmail.com>
2021-05-26 15:13:44 +0200

    Tobit predictions

--- Modelling/Tobit_model_creation.py
+++ Modelling/Tobit_model_creation.py
@@ -41,20 +41,45 @@ def censor(s):
 
 def tobit_model(label):
     df = load_data()
+    df = df.drop(columns=['Charging Time (mins)', 'Parking Time (mins)','# Food', '# Shop & Service',
+       '# Professional & Other Places', '# Nightlife Spot',
+       '# Outdoors & Recreation', '# Arts & Entertainment',
+       '# Travel & Transport', '# College & University', '# Event',
+       '# Residence', 'Plugs_raw', 'Number of NEMA 5-20R_raw','Number of NEMA 5-20R', 'Label_0', 'Label_1', 'Label_2', 'Label_3',
+       'Label_4', 'Label_5', 'Label_6', 'Label_7'])
+    df = df[df["Label"]==label]
+    df1 = pd.read_csv("data\createdDat\Censorship_scheem\latent1_26052021.csv")
+    df1 = df1[df1["Label"]==label]
+    df1["Date"]= pd.to_datetime(df1["Date"])
+    df1 = df1.set_index("Date")
     l = lags()
     testdata = df[df.Label == label]
+    index = testdata[testdata["Energy (kWh)"]>0].index[0]
+    testdata = testdata[index:] 
+    index = df[df["Energy (kWh)"]>0].index[0]
+    df= df[index:]
 
     scaler_eng = StandardScaler()
     scaler_fee = StandardScaler()
-    testdata["Energy (kWh)"] = scaler_eng.fit_transform(np.array(testdata["Energy (kWh)"]).reshape(-1,1))
-    testdata["Fee (USD)"] = scaler_fee.fit_transform(np.array(testdata["Fee (USD)"]).reshape(-1,1))
 
     for i in range(1,13):
         testdata[f"Year_Month_{i}"] = 0
         testdata[f"Year_Month_{i}"][(testdata.index.month==i) & (testdata.index.day==1)] = 1
         testdata[f"Year_Month_{testdata.index[0].month}"]=1
+    
+    testdata["Censored"] = False
+    testdata["Censored"][(testdata["Label"]==label) & (df1["Plugs"]==df1["Simultaneous use (daily max)_raw"])] = True
+
+
+    #df["Censored"] = False
+    #df["Censored"][(df["Label"]==label) & (df1["Plugs"]==df1["Simultaneous use (daily max)_raw"])] = True
+    #df["Energy (kWh)"][df["Censored"]==True] = df["Energy (kWh)"][df["Censored"]==True] * 1.2
+
+    testdata["Energy (kWh)"] = scaler_eng.fit_transform(np.array(testdata["Energy (kWh)"]).reshape(-1,1))
+    testdata["Fee (USD)"] = scaler_fee.fit_transform(np.array(testdata["Fee (USD)"]).reshape(-1,1))
 
     testdata = l.buildLaggedFeatures(testdata,["Energy (kWh)"])
+
     testdata = testdata.apply(censor, axis=1)
 
     t = Tobit(testdata, 'Censored', list(testdata.drop(columns=["Energy (kWh)","Censored"]).columns), 'Energy (kWh)')
@@ -72,20 +97,29 @@ def tobit_model(label):
 
     pred_tobit = t.predict(np.array(testdata.drop(columns=["Energy (kWh)","Censored"])),beta)
 
-    rmse_tobit = np.sqrt(mean_squared_error(df["Energy (kWh)"][(df.Label == label)][5:],scaler_eng.inverse_transform(pred_tobit)))
-    rmse_ridge = np.sqrt(mean_squared_error(df["Energy (kWh)"][(df.Label == label)][5:],scaler_eng.inverse_transform(pred)))
+    rmse_tobit = np.sqrt(mean_squared_error(df["Energy (kWh)"][5:],scaler_eng.inverse_transform(pred_tobit)))
+    rmse_ridge = np.sqrt(mean_squared_error(df["Energy (kWh)"][5:],scaler_eng.inverse_transform(pred)))
 
     cmap = plt.cm.bone
     rmap = plt.cm.Reds
     bmap = plt.cm.Blues
 
-    plt.plot(df["Energy (kWh)"][df.Label == label], label = "Original", alpha = 0.6, color="black")
-    plt.plot(testdata.index,scaler_eng.inverse_transform(pred_tobit), label = "Tobit", alpha = 0.6, color=rmap(0.9))
-    plt.plot(testdata.index,scaler_eng.inverse_transform(pred), label = "Ridge", alpha = 0.6, color=bmap(0.9))
-    plt.xlabel("Date")
-    plt.ylabel("Energy (kWh)")
-    plt.title(f"Cluster {label}")
-    plt.legend()
-    plt.show()
+    # plt.plot(df["Energy (kWh)"][5:], label = "Original", alpha = 0.6, color="black")
+    # plt.plot(testdata.index,scaler_eng.inverse_transform(pred_tobit), label = "Tobit", alpha = 0.6, color=rmap(0.9))
+    # plt.plot(testdata.index,scaler_eng.inverse_transform(pred), label = "Ridge", alpha = 0.6, color=bmap(0.9))
+    # plt.xlabel("Date")
+    # plt.ylabel("Energy (kWh)")
+    # plt.title(f"Cluster {label}")
+    # plt.legend()
+    # plt.show()
 
-tobit_model(0)
+    predictions = pd.DataFrame(columns=["Tobit_pred","Ridge_pred", "Censored_energy"])
+    #predictions = predictions.set_index(testdata.index)
+    predictions["Tobit_pred"] = scaler_eng.inverse_transform(pred_tobit)
+    predictions["Ridge_pred"] = scaler_eng.inverse_transform(pred)
+    predictions["Censored_energy"] = testdata["Energy (kWh)"]
+
+    return predictions
+
+for label in range(8):
+    tobit_model(label).to_csv(f"data\createdDat\Censorship_scheem\Tobit_0.5_censor_label{label}.csv")
